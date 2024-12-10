47402ea88344b6189f0dcff8ab65054013905cce
gax <gax@clifford-the-big-red-macbook.local>
2024-12-10 18:39:48 -0500

    work in progress (broken)

---
+++ routing.js
@@ -0,0 +1,27 @@
+function submit() {
+    var rwd = document.getElementById('rwd');
+    var fwd = document.getElementById('fwd');
+    var anydt = document.getElementById('anydt');
+    
+
+    var urls = [];
+
+    if (rwd.checked) {
+        urls.push("https://onlinesequencer.net/members/25482");
+    }
+    if (fwd.checked) {
+        urls.push(fwd.value);
+    }
+    if (anydt.checked) {
+        urls.push(anydt.value);
+    }    
+  
+
+    urls.forEach(function(url) {
+        goToUrl(url);
+    });
+}
+
+function goToUrl(url) {
+    var win = window.open(url, '_blank');
+}
